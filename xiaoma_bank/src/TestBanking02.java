f4470c7730936172069b61d39e8e09174e96c734
MaBuyao <860977475@qq.com>
2023-09-08 09:22:17 +0800

    mby0908

---
+++ xiaoma_bank/src/TestBanking02.java
@@ -0,0 +1,45 @@
+/**
+ * ClassName: TestBanking02
+ * Description:
+ *
+ * @Author Mabuyao
+ * @Create 2023/9/7 17:06
+ * @Version 1.0
+ */
+import com.xiaoma.banking.Account;
+import com.xiaoma.banking.Customer;
+
+public class TestBanking02 {
+
+//    public static void main(String[] args) {
+//        Customer customer;
+//        Account account = new Account(500);
+//
+//        // Create an account that can has a 500.00 balance.
+//        System.out.println("Creating the customer Jane Smith.");
+//        //code
+//        customer = new Customer("Jane","Smith");
+//
+//        System.out.println("Creating her account with a 500.00 balance.");
+//        //code
+//        customer.setAccount(account);
+//
+//        System.out.println("Withdraw 150.00");
+//        //code
+//        customer.getAccount().withdraw(150.00);
+//
+//        System.out.println("Deposit 22.50");
+//        //code
+//        customer.getAccount().deposit(22.50);
+//
+//        System.out.println("Withdraw 47.62");
+//        //code
+//        customer.getAccount().withdraw(47.62);
+//
+//        // Print out the final account balance
+//        System.out.println("Customer [" + customer.getLastName()
+//                + ", " + customer.getFirstName()
+//                + "] has a balance of " + account.getBalance());
+//    }
+}
+
